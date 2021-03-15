e45577514b8848a56221a4f42b2a4d987e47537f
selimbenmrad <selimbenmrad@hotmail.com>
2021-03-15 10:01:03 +0100

    selim ajouter facebook et admob a la confidentialite

--- src/Confidentiality.js
+++ src/Confidentiality.js
@@ -7,6 +7,12 @@ function Assistance() {
     <h1>confidentiality polic </h1>
 
     <p> 1- All user information are used by our company only for only prupose of identify the user, the data will no be shared not used for any commercial use</p>
+    <p> 2- Advirtisement rely on ad mob, which an advirtisiment service provided by google, more information can be found here:
+    https://admob.google.com/home/
+    </p>
+
+    <p> 2- As part of our advirtisement strategy, we use facebook advirtisiement, more information about their policies can be found herehttps://www.facebook.com/business/ads.
+    </p>
 
       <p> The data are hosted on firebase server, that is the own by alphabet inc ( google)</p>
    <p><b> Email adress:</b> fullhealthcompanion@gmail.com </p>
