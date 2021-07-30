1005214e9f86a631ffd0a4b490791850d7465c49
selim ben mrad <selimbenmrad@selims-Mac-mini.local>
2021-07-30 10:40:52 +0100

    selim  ajouter des detail

--- src/Confidentiality.js
+++ src/Confidentiality.js
@@ -2,27 +2,28 @@ import React from 'react';
 
 function Assistance() {
   return (
-  
     <body>
     <h1>confidentiality policy </h1>
 
-    <p> 1- All user information are used by our company only for internal purposes like identifying the user, the data will no be shared not used for any external commercial use</p>
-    <p> 2- Advertisement relies on ad mob, which an advertising service provided by Google, more information can be found here:
-    https://admob.google.com/home/
+ 
+    <p> 2- Advertisement relies on ad mob, which is an advertising service provided by Google,therefore user information will be shared with the company  more information can be found here:
+    https://admob.google.com/home/. the company will use customer data and sell them for business purposes.
     </p>
 
-    <p> 2- As part of our advertisement strategy, we use a Facebook advertisement, more information about their policies can be found herehttps://www.facebook.com/business/ads.
-    </p>
-
-
-    <p> 3- We use revenuecat payment services, therefore user information will be shared with the company revenuecat, more information can be found here https://www.revenuecat.com/.
-    </p>
-
-    <p> 2- As part of our tracking and allocating advirtisement information, we use appsflyer services, more information about their policies can be found here https://www.appsflyer.com/.
+    <p> 2- As part of our advertisement strategy, we use a Facebook advertisement,therefore user information will be shared with the company more information about their policies can be found herehttps://www.facebook.com/business/ads. the company will use customer data and sell them for business purposes.
     </p>
 
 
-      <p> The data are hosted on firebase server, that is the own by alphabet inc ( google)</p>
+    <p> 3- We use revenue payment services, therefore user information will be shared with the company , more information can be found here https://www.revenuecat.com/ .the company will use customer data and sell them for business purposes.
+    </p>
+
+    <p> 2- As part of our tracking and allocating advertisement information, we use AppsFlyer services,therefore user information will be shared with the company more information about their policies can be found here https://www.appsflyer.com/. the company will use customer data and sell them for business purposes.
+    </p>
+
+
+      <p> The data are hosted on firebase server, which is owned by alphabet inc ( google), user information might be used for business purposes.</p>
+
+   <p> 1- Our company will use user information only for internal purposes like identifying the user, the data will no be shared not used for any external commercial use</p>
    <p><b> Email adress:</b> fullhealthcompanion@gmail.com </p>
       </body>
   );
